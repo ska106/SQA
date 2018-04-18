07290249a4107214ecba77bab48b9a90b2beeb7c
Sudeep Agrawal <Sudeep.Agrawal@gmail.com>
2018-04-18 15:33:32 -0400

    Changes made to Baseline to generate parts for multipart upload.

--- S3Utils/src/com/sqacanada/SQACanUtils.java
+++ S3Utils/src/com/sqacanada/SQACanUtils.java
@@ -11,12 +11,15 @@ public class SQACanUtils
 		return uploadId.replace("<UploadId>", "").replace("</UploadId>", "");		
 	}//getUploadId
 	
+	/*
+	* For Local Test-purposes only.
+	*/
 	public static void main (String a[])
 	{
 		System.out.println(SQACanUtils.getUploadId("<UploadId>ZuBsBV83T6qf_6PukwiDTg</UploadId>"));
 		System.out.println(SQACanUtils.getFQNFileName("Path1", "F1"));
 		System.out.println(SQACanUtils.makePartTag("1", "eTag"));
-	}
+	}//main
 	
 	/**
 	 * @param path
@@ -31,20 +34,23 @@ public class SQACanUtils
 		return fqnFilename.toString();
 	}//getFQNFileName
 	
-	/*<CompleteMultipartUpload>
-  <Part>
-    <PartNumber>1</PartNumber>
-    <ETag>"a54357aff0632cce46d942af68356b38"</ETag>
-  </Part>
-  <Part>
-    <PartNumber>2</PartNumber>
-    <ETag>"0c78aef83f66abc1fa1e8477f296d394"</ETag>
-  </Part>
-  <Part>
-    <PartNumber>3</PartNumber>
-    <ETag>"acbd18db4cc2f85cedef654fccc4a4d8"</ETag>
-  </Part>
-</CompleteMultipartUpload>*/
+	/*
+	 * This metod creats an XML in the following format :
+	 *<CompleteMultipartUpload>
+	 *  <Part>
+	 *		<PartNumber>1</PartNumber>
+     *			<ETag>"a54357aff0632cce46d942af68356b38"</ETag>
+     *		  </Part>
+	 * <Part>
+	 *	<PartNumber>2</PartNumber>
+	 *	<ETag>"0c78aef83f66abc1fa1e8477f296d394"</ETag>
+	 * </Part>
+	 * <Part>
+	 *	<PartNumber>3</PartNumber>
+	 *	<ETag>"acbd18db4cc2f85cedef654fccc4a4d8"</ETag>
+	 * </Part>
+	 *</CompleteMultipartUpload>
+	*/
 	public static String makePartTag(String partNumber, String eTag)
 	{
 		StringBuffer returnString = new StringBuffer("<Part>").
@@ -52,5 +58,5 @@ public class SQACanUtils
 										append("<ETag>\"").append(eTag).append("\"</ETag>").
 										append("</Part>");
 		return returnString.toString();
-	}
+	}//makePartTag
 }//SQACanUtils
